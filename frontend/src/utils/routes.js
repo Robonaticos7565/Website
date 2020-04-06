96e48a6484e1c6ec3fd1b9ced07bbbe17f2e0e20
Matheus Santos <matheus.psantos2016@gmail.com>
2020-04-06 18:51:22 -0300

    routes.js added

---
+++ frontend/src/utils/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Competitions from '../pages/Competitions';
+import Projects from '../pages/Projects';
+import UnderConstruction from '../pages/UnderConstruction';
+import NotFound from '../pages/NotFound';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/projects' component={Projects} />
+                <Route path='/about' component={About} />
+                <Route path='/competition' component={Competitions} />
+                <Route component={NotFound} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
