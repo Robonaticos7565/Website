00f52926373654fec6d5b01b2e1660b0b9ad5769
Matheus Santos <matheus.psantos2016@gmail.com>
2020-04-07 20:53:23 -0300

    Home page started

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,12 +1,48 @@
 import React, { useEffect } from 'react';
 
+import { Window, Container, Content } from '../../components/Base'
+import { Title } from '../../components/Text';
+import { Menu } from '../../components/Menu'
+
 export default function Home() {
 
-    useEffect (() => {
-        document.title = `Home`;
+    useEffect(() => {
+        document.title = `Robonáticos - Home`;
     })
 
     return (
-        <h1>Home</h1>
+        <Window>
+            <Container>
+                <Menu/>
+                <Content>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                    <Title>Home</Title>
+                </Content>
+            </Container>
+        </Window>
     );
 }
