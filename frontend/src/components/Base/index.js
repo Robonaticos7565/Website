94b38efc48760d9f2c74760620e71f4bcd95cce2
Matheus Santos <matheus.psantos2016@gmail.com>
2020-04-08 14:06:32 -0300

    Container responsaive finished

--- frontend/src/components/Base/index.js
+++ frontend/src/components/Base/index.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import colors, { hex2rgb } from '../../res/colors';
+import devices from '../../res/devices';
 
 export const Window = styled.div`
     position: relative;
@@ -14,7 +15,13 @@ export const Container = styled.div`
     width: 100%;
     max-width: 1500px;
     padding-left: 7rem;
+    padding-top: 0;
     margin: 0 auto;
+
+    @media ${devices.laptop} {
+        padding-left: 0;
+        padding-top: 5rem;
+    }
 `;
 
 export const Content = styled.div`
