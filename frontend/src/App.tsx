02f0bc8fde84a7025fcc39dcff030ffd8574d14d
Matheus Santos <matheus.psantos2016@gmail.com>
2020-04-09 18:58:00 -0300

    toggle theme started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,19 @@
 import React, { useState, createContext } from 'react';
 import Routes from './utils/routes';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import usePersistedState from './utils/usePersistedState';
+
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
 
 import GlobalStyle from './styles/global';
 
 export default function App() {
-  const [theme, setTheme] = useState(light)
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light)
 
   const toggleTheme = () => {
     setTheme(theme.title === 'light' ? dark : light)
   }
-
-  toggleTheme() = React.
 
   return (
     <ThemeProvider theme={dark}>
