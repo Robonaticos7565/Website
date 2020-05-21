714e24949d303604f85b03548552b3211583f2c2
Matheus Santos <matheus.psantos2016@gmail.com>
2020-05-21 19:19:17 -0300

    colors props added

--- frontend/src/styles/colors.ts
+++ frontend/src/styles/colors.ts
@@ -1,22 +1,59 @@
-const colors = {
-    mainYellow : '#F7C01F',
-    gray33 : '#333333',
-    gray54 : '#545454',
-    white : '#FFFFFF',
+export default {
+    mainYellow: '#F7C01F',
+    gray33: '#333333',
+    gray54: '#545454',
+    white: '#FFFFFF',
     whiteF0: '#F0F0F0',
-    black : '#000000',
+    black: '#000000',
 }
 
-export default colors;
+export interface ColorsPropsBackground {
+    primary: string;
+    secondary: string;
+    tertiary: string;
+    error: string;
+}
+
+export interface ColorsPropsText {
+    primary: string;
+    secondary: string;
+    tertiary: string;
+    fourth: string;
+    disabled: string;
+    white: string;
+}
+
+export type ButtonColorsScheme = {
+    borderColor?: string;
+    backgroundColor?: string;
+    textColor?: string;
+}
+
+export interface ColorsPropsButton {
+    def: ButtonColorsScheme;
+}
+
+export interface ColorsProps {
+    background: ColorsPropsBackground;
+    text: ColorsPropsText;
+    button: ColorsPropsButton;
+}
+
+// General export for components
+export type ColorsBackgroundOptions = keyof ColorsPropsBackground;
+
+export type ColorsTextOptions = keyof ColorsPropsText;
+
+export type ColorsButtonOptions = keyof ColorsPropsButton;
 
 export function hex2rgb(hex: string, opacity: Number) {
-    var h=hex.replace('#', '') as any;
-    h =  h.match(new RegExp('(.{'+h.length/3+'})', 'g'));
+    var h = hex.replace('#', '') as any;
+    h = h.match(new RegExp('(.{' + h.length / 3 + '})', 'g'));
 
-    for(var i=0; i<h.length; i++)
-        h[i] = parseInt(h[i].length==1? h[i]+h[i]:h[i], 16);
+    for (var i = 0; i < h.length; i++)
+        h[i] = parseInt(h[i].length == 1 ? h[i] + h[i] : h[i], 16);
 
-    if (typeof opacity != 'undefined')  h.push(opacity);
+    if (typeof opacity != 'undefined') h.push(opacity);
 
-    return 'rgba('+h.join(',')+')';
+    return 'rgba(' + h.join(',') + ')';
 }
