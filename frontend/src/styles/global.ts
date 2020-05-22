0a3cdc997c01a12aed05923423777bcaf8ee9a11
Matheus Santos <matheus.psantos2016@gmail.com>
2020-05-22 18:19:07 -0300

    global.ts updated

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -1,18 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
-// import * as Roboto from '../assets/fonts/Roboto/Roboto-Bold.ttf'
-
 export default createGlobalStyle`
 
-    /* @font-face {
-        font-family: 'Roboto Bold';
-        src: local('Roboto'), local('Roboto'), url('../assets/fonts/Roboto/Roboto-Bold.ttf') format('ttf'),
-        src: url('../assets/fonts/Roboto/Roboto-Bold.ttf'); 
-    } */
-
-    :root {
-        font-size: 16px;
-    }
+    :root { font-size: 16px; }
 
     * {
         margin: 0;
@@ -28,11 +18,7 @@ export default createGlobalStyle`
         -webkit-font-smoothing: antialiased;
     }
 
-    ul {
-        list-style: none;
-    }
+    ul { list-style: none; }
 
-    a {
-        text-decoration: none;
-    }
+    a { text-decoration: none; }
 `;
